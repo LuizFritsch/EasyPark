5cf93b702dc34540c41e1b1f2944e2c01b7e3193
Luiz Fritsch <fritsch.guilherm3@gmail.com>
2019-09-23 08:26:29 -0300

    Add files via upload

---
+++ EasyPark_v1.ino
@@ -0,0 +1,42 @@
+//Programa: Conectando Sensor Ultrassonico HC-SR04 ao Arduino
+//Autor: FILIPEFLOP
+ 
+//Carrega a biblioteca do sensor ultrassonico
+#include <Ultrasonic.h>
+ 
+//Define os pinos para o trigger e echo
+#define pino_trigger 5
+#define pino_echo 4
+ 
+//Inicializa o sensor nos pinos definidos acima
+Ultrasonic ultrasonic(pino_trigger, pino_echo);
+ 
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Lendo dados do sensor...");
+}
+ 
+void loop()
+{
+  //Le as informacoes do sensor, em cm e pol
+  float cmMsec, inMsec;
+  long microsec = ultrasonic.timing();
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+  //Exibe informacoes no serial monitor
+  Serial.print("Distancia em cm: ");
+  Serial.print(cmMsec);
+  Serial.print(" - Distancia em polegadas: ");
+  Serial.println(inMsec);
+  delay(3000);
+  if(cmMsec>10){
+    digitalWrite(3, HIGH);
+    digitalWrite(2, LOW);
+  }else{
+      digitalWrite(2, HIGH);
+      digitalWrite(3, LOW);
+  }
+
+  
+}
